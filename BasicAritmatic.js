d445f1bf725e37ddfebc8ef90f5c894659cb211b
nikita-pandav <140898391+nikita-pandav@users.noreply.github.com>
2024-03-26 22:38:10 -0900

    Add files via upload

---
+++ BasicAritmatic.js
@@ -0,0 +1,84 @@
+// console.log("Hello World");
+
+// 1. Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.
+// var a = 10;
+// var b = 5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// 2. Take two numbers and only print the integer part when a is divided by b.
+// let a = 10;
+// let b = 3;
+// console.log(parseInt(a/b));
+
+// 3. Take two numbers a,b and print the exponential power of the first number raised by the second.
+// console.log(a**b);
+
+// 4. Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+// let a = 10;
+// let b = 3;
+// let c = a/b;
+// console.log(c%1);
+
+// 5. Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+// let a = 60;
+// let b = 80;
+// let c = 50;
+// let d = 90;
+// let e = 40;
+// let sum = a+b+c+d+e;
+// let average = sum/5;
+// console.log(average);
+
+// 6. Take the cost price a and selling price b of a product, and print the profit obtained on the product.
+// let sp = 15;
+// let cp = 10;
+// let profit = sp-cp;
+// console.log(profit);
+
+// 7. Take a number and print out the last digit of the number.
+// let num = 234;
+// let lastdigit = 234%10;
+// console.log(lastdigit);
+
+// 8. Take a 2 digit number and return the first and last digits.
+// let n = 23;
+// let fistdigit = parseInt(n/10);
+// let lastdigit = n%10;
+// console.log(fistdigit,lastdigit);
+
+// 9. Write a JavaScript program to find the largest of three numbers.
+// let num1 = 20;
+// let num2 = 30;
+// let num3 = 10;
+// let largest = num1;
+// if (num2 > largest){
+//     largest = num2
+// }
+// else if(num3 > largest){
+//     largest = num3
+// }
+// console.log(largest)
+
+// 10. Write a JavaScript program to check whether a number is even or odd.
+// let num = 23;
+// if (num%2 == 0){
+//     console.log("Even No");
+// }
+// else{
+//     console.log("Odd No");
+// }
+
+// 11. Write a JavaScript program to check whether a given number is prime or not.
+
+// 12. Write a JavaScript program to convert temperature from Celsius to Fahrenheit and vice versa.
+// let Celsius = 30;
+// let Fahrenheit = (Celsius*(9/5))+32;
+// console.log(Celsius+"°C is "+Fahrenheit+"°F");
+
+// let F = 68;
+// let C = (F-32)*5/9;
+// console.log(F+"°C is "+C+"°F");
